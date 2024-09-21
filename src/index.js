e2a764c5283f65798eff9990bfd3ecd4a90112de
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-21 20:25:11 +0300

    add app initial functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./styles.css";
 
+import { DomHandler } from "./modules/domhandler.js";
+import { RequestHandler } from "./modules/requesthandler.js";
+
+RequestHandler.makeRequest(DomHandler.getInputValue());
+DomHandler.assignEnterClick();
